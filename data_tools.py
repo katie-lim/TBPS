10ae603baa64b4c7df691876726a8582f35a04d0
daryl-lee <92518556+daryl-lee@users.noreply.github.com>
2021-10-30 17:44:12 +0100

    Update data_tools.py

--- data_tools.py
+++ data_tools.py
@@ -43,7 +43,7 @@ def separate_dataset(df, columns_idx, prob=0.95):
 
 
 
-def filter_dataset(df, prob=0.95, PKMuMu_OWNPV_filter = 9, K_s_ratio = 9, mu_pt_lim = 300, \
+def filter_dataset(df, prob=0.3, PKMuMu_OWNPV_filter = 9, K_s_ratio = 9, mu_pt_lim = 300, \
                                 B0_dira = 0.9995, B0_ratio = 9, B0_ipcs_opv_lim = 25, B0_fdcs_opv_lim = 100):
     """
     Filters the DataFrame by only keeping the rows where we are confident that the mu plus and mu minus are indeed muons, the K is indeed a kaon, and the pi is a pion.
@@ -56,10 +56,10 @@ def filter_dataset(df, prob=0.95, PKMuMu_OWNPV_filter = 9, K_s_ratio = 9, mu_pt_
     Params
     ------
     df: pandas.DataFrame containing the data
-    prob: float defining the cutoff probability at which we keep/discard rows (set to 0.95 by default)
+    prob: float defining the cutoff probability at which we keep/discard rows (set to 0.3 by default, approx equal to 0.8^5)
     PKMuMu_OWNPV_filter: Defines the cutoff value for the IPCHI2_OWNPV values of the data for the: Kaon, pion, and muons.
     K_s_ratio: Defines the cutoff value for the Kstar_ENDVERTEX_CHI2/Kstar_ENDVERTEX_NDOF.
-    mu_pt_lim: Defines the cutoff value for mu_plus_PT and mu_minus_PT - the muon total momentum in MeV/c^2.
+    mu_pt_lim: Defines the cutoff value for mu_plus_PT and mu_minus_PT - the muon transverse momentum in MeV/c^2.
     B0_dira: Defines the cutoff value for B0_DIRA_OWNPV - the cosine between the momentum and flight vectors.
     B0_ratio: Defines the cutoff value for B0_ENDVERTEX_CHI2/B0_ENDVERTEX_NDOF.
     B0_ipcs_opv_lim: Defines the cutoff value of B0_IPCHI2_OWNPV.
@@ -75,14 +75,20 @@ def filter_dataset(df, prob=0.95, PKMuMu_OWNPV_filter = 9, K_s_ratio = 9, mu_pt_
     column_list_mu_plus = ["mu_plus_MC15TuneV1_ProbNNmu", "mu_plus_MC15TuneV1_ProbNNk", "mu_plus_MC15TuneV1_ProbNNpi", "mu_plus_MC15TuneV1_ProbNNe", "mu_plus_MC15TuneV1_ProbNNp"]
     column_list_mu_minus = ["mu_minus_MC15TuneV1_ProbNNmu", "mu_minus_MC15TuneV1_ProbNNk", "mu_minus_MC15TuneV1_ProbNNpi", "mu_minus_MC15TuneV1_ProbNNe", "mu_minus_MC15TuneV1_ProbNNp"]
     column_list_K = ["K_MC15TuneV1_ProbNNk","K_MC15TuneV1_ProbNNmu", "K_MC15TuneV1_ProbNNpi", "K_MC15TuneV1_ProbNNe", "K_MC15TuneV1_ProbNNp"]
-    column_list_Pi = ["Pi_MC15TuneV1_ProbNNp", "Pi_MC15TuneV1_ProbNNk","Pi_MC15TuneV1_ProbNNmu", "Pi_MC15TuneV1_ProbNNpi", "Pi_MC15TuneV1_ProbNNe"]
+    column_list_Pi = ["Pi_MC15TuneV1_ProbNNpi", "Pi_MC15TuneV1_ProbNNk","Pi_MC15TuneV1_ProbNNmu", "Pi_MC15TuneV1_ProbNNp", "Pi_MC15TuneV1_ProbNNe"]
     
     column_list = [column_list_mu_plus, column_list_mu_minus, column_list_K, column_list_Pi]
     
-    
+    pf_df = df.copy()
     #I have structured the column lists such that when put through this for loop the elements are in the appropriate column
     for i in column_list:
-        pf_df = df[((df[i[0]]) * (1 - df[i[1]]) * (1 - df[i[2]]) * (1 - df[i[3]])) > prob]
+        #set -ve probabilities to 0
+        pf_df[i[0]][pf_df[i[0]] < 0] = 0
+        pf_df[i[1]][pf_df[i[1]] < 0] = 0
+        pf_df[i[2]][pf_df[i[2]] < 0] = 0
+        pf_df[i[3]][pf_df[i[3]] < 0] = 0
+        pf_df[i[4]][pf_df[i[4]] < 0] = 0
+        pf_df = pf_df[((pf_df[i[0]]) * (1 - pf_df[i[1]]) * (1 - pf_df[i[2]]) * (1 - pf_df[i[3]]) * (1 - pf_df[i[4]])) > prob]
 
 
 
