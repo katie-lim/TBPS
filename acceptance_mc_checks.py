b24fa1c76bc0cd00de57e083609aa21648d1aac8
AlexanderSinclairTeixeira <alesintex@gmail.com>
2021-11-07 20:42:55 +0000

    Bug fixed and updated call signatures

--- acceptance_mc_checks.py
+++ acceptance_mc_checks.py
@@ -1,40 +1,40 @@
 """
-This is to run some basic checks on the sim data so I can see what we are working with.
+This is to run some basic checks on the sim and actual data so I can see what we are working with.
 """
 
-import pandas as pd
 import matplotlib.pyplot as plt
-from data_tools import filter_dataset
+from data_tools_and_filters import make_clean_df
+from filtering_process import filters_and_params, filter_and_log_index, lhc_data_fpath, sim_data_fpath
 
-particles = ['mu_plus_', 'mu_minus_', 'K_', 'Pi_']
-p_col_names = ['P', 'PT', 'PE', 'PX', 'PY', 'PZ']
 
-df_sim = pd.read_pickle('data/acceptance_mc.pkl').fillna(0)
-df_sim_filtered = filter_dataset(df_sim)
-df_tot = pd.read_pickle('data/total_dataset.pkl').fillna(0)
-df_tot_filtered = filter_dataset(df_tot)
+lhc_df = make_clean_df(lhc_data_fpath)
+sim_df = make_clean_df(sim_data_fpath)
+
+lhc_log = filter_and_log_index(lhc_df, filter_process=filters_and_params, mode='cumulative')
+sim_log = filter_and_log_index(sim_df, filter_process=filters_and_params, mode='cumulative')
+
+lhc_df_filtered = lhc_df.loc[lhc_log['Final']]
+sim_df_filtered = sim_df.loc[sim_log['Final']]
 
 if True:
-    fig, axs = plt.subplots(2, 2, figsize=[10,8])
-    axs[0,0].hist(df_tot['costhetal'])
-    axs[0,0].set_xlabel(r'cos($\theta_l$)')
-    axs[0,0].set_ylabel('N')
-    axs[0,0].set_title('Total dataset')
+    fig, axs = plt.subplots(2, 2, figsize=[10, 8])
+    axs[0, 0].hist(lhc_df['costhetal'])
+    axs[0, 0].set_xlabel(r'cos($\theta_l$)')
+    axs[0, 0].set_ylabel('N')
+    axs[0, 0].set_title('Total dataset')
 
-    axs[0,1].hist(df_sim['costhetal'])
-    axs[0,1].set_xlabel(r'cos($\theta_l$)')
-    axs[0,1].set_ylabel('N')
-    axs[0,1].set_title('Simulated dataset')
+    axs[0, 1].hist(sim_df['costhetal'])
+    axs[0, 1].set_xlabel(r'cos($\theta_l$)')
+    axs[0, 1].set_ylabel('N')
+    axs[0, 1].set_title('Simulated dataset')
 
-    h, bins, patches = axs[1,0].hist(df_tot_filtered['costhetal'])
-    axs[1,0].set_xlabel(r'cos($\theta_l$)')
-    axs[1,0].set_ylabel('N')
-    axs[1,0].set_title('Total dataset - filtered')
+    axs[1, 0].hist(lhc_df_filtered['costhetal'])
+    axs[1, 0].set_xlabel(r'cos($\theta_l$)')
+    axs[1, 0].set_ylabel('N')
+    axs[1, 0].set_title('Total dataset - filtered')
 
-    axs[1,1].hist(df_sim_filtered['costhetal'])
-    axs[1,1].set_xlabel(r'cos($\theta_l$)')
-    axs[1,1].set_ylabel('N')
-    axs[1,1].set_title('Simulated dataset - filtered')
+    axs[1, 1].hist(sim_df_filtered['costhetal'])
+    axs[1, 1].set_xlabel(r'cos($\theta_l$)')
+    axs[1, 1].set_ylabel('N')
+    axs[1, 1].set_title('Simulated dataset - filtered')
     fig.tight_layout()
-
-
