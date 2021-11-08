b9481262d9d74e57041e6a2a93c7f20893771f9a
katie-lim <35044591+katie-lim@users.noreply.github.com>
2021-11-08 10:31:51 +0000

    Insert acceptance functions into skeleton code, obtaining fits for F_L and A_FB
    - added function to split data into q2 bins
    - moved acceptance code into functions
    - inserted acceptance functions into skeleton code, obtaining preliminary values for F_L and A_FB

--- data_tools.py
+++ data_tools.py
@@ -47,7 +47,7 @@ def filter_dataset(df, prob=0.3, PKMuMu_OWNPV_filter = 9, K_s_ratio = 25, mu_pt_
     """
     Filters the DataFrame by only keeping the rows where we are confident that the mu plus and mu minus are indeed muons, the K is indeed a kaon, and the pi is a pion.
     The cutoff probability is defined by the parameter called prob.
-    
+
     It subsequently filters the data to ensure the daughter particles truly came from the decay vertex, and that the B0 came from the primary vertex.
     The paramaters were taken from this academic paper: http://www.ep.ph.bham.ac.uk/publications/thesis/lxp_thesis.pdf
 
@@ -63,7 +63,7 @@ def filter_dataset(df, prob=0.3, PKMuMu_OWNPV_filter = 9, K_s_ratio = 25, mu_pt_
     B0_ratio: Defines the cutoff value for B0_ENDVERTEX_CHI2/B0_ENDVERTEX_NDOF.
     B0_ipcs_opv_lim: Defines the cutoff value of B0_IPCHI2_OWNPV.
     B0_fdcs_opv_lim: Defines the cutoff value of B0_FDCHI2_OWNPV.
-    
+
 
     Returns
     ------
@@ -75,9 +75,9 @@ def filter_dataset(df, prob=0.3, PKMuMu_OWNPV_filter = 9, K_s_ratio = 25, mu_pt_
     column_list_mu_minus = ["mu_minus_MC15TuneV1_ProbNNmu", "mu_minus_MC15TuneV1_ProbNNk", "mu_minus_MC15TuneV1_ProbNNpi", "mu_minus_MC15TuneV1_ProbNNe", "mu_minus_MC15TuneV1_ProbNNp"]
     column_list_K = ["K_MC15TuneV1_ProbNNk","K_MC15TuneV1_ProbNNmu", "K_MC15TuneV1_ProbNNpi", "K_MC15TuneV1_ProbNNe", "K_MC15TuneV1_ProbNNp"]
     column_list_Pi = ["Pi_MC15TuneV1_ProbNNpi", "Pi_MC15TuneV1_ProbNNk","Pi_MC15TuneV1_ProbNNmu", "Pi_MC15TuneV1_ProbNNp", "Pi_MC15TuneV1_ProbNNe"]
-    
+
     column_list = [column_list_mu_plus, column_list_mu_minus, column_list_K, column_list_Pi]
-    
+
     pf_df = df.copy()
     #I have structured the column lists such that when put through this for loop the elements are in the appropriate column
     for i in column_list:
@@ -112,28 +112,41 @@ def filter_dataset(df, prob=0.3, PKMuMu_OWNPV_filter = 9, K_s_ratio = 25, mu_pt_
 def filter_dataset_by_pt(df, p_ratio = 0.1):
     """
     Filters the DataFrame by removing rows with particles that have small transverse momentum
-    
+
     Size of the transverse momentum is quantified by taking the ratio of the transverse momentum to its momentum in the z direction
-    
+
     Particles that have a smaller ratio than p_ratio are removed from the dataset
-    
+
     Params
     ------
     df: pandas.DataFrame containing the data
     sig_level: float defining the cutoff ratio at which we keep/discard rows (set to 0.1 by default)
-    
+
     Returns
     ------
     pandas.DataFrame containing the filtered data
     """
-    
+
     particles = ["mu_plus", "mu_minus", "K", "Pi"]
-    
+
     filtered_df = df.copy()
-    
+
     #iterate ratio calculation over all particles
     for particle in particles:
         filtered_df["{}_p_ratio".format(particle)] = filtered_df["{}_PT".format(particle)]/filtered_df["{}_PZ".format(particle)]
         filtered_df = filtered_df[filtered_df["{}_p_ratio".format(particle)] > p_ratio]
-        
+
     return filtered_df
+
+
+
+def split_into_bins(df):
+
+    # define the ranges of q^2 for each bin, in line with
+    # https://mesmith75.github.io/ic-teach-kstmumu-public/predictions/
+    bin_ranges = [(0.1, 0.98), (1.1, 2.5), (2.5, 4.0), (4.0, 6.0), (6.0, 8.0), (15.0, 17.0), (17.0, 19.0), (11.0, 12.5), (1.0, 6.0), (15.0, 17.9)]
+
+
+    bins = [df[(df["q2"] >= bin_range[0]) & (df["q2"] <= bin_range[1])] for bin_range in bin_ranges]
+
+    return bins
